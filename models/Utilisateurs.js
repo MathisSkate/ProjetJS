1103ac11df97d87e91fc339e1c576d8d098d58ec
Mathis <mathisleskateur@gmail.com>
2022-02-01 16:17:27 +0100

    Ajouter du model utilisateur

---
+++ models/Utilisateurs.js
@@ -0,0 +1,14 @@
+// Importation du module mongoose de MongoDB
+//Mongoose permet de créer un Shema des données enregistrer dans MongoDB
+const mongoose = require('mongoose');
+
+//Création d'un Shema de données de l'utilisateur à enregistrer dans MongoDB
+const utilisateurSchema = mongoose.Schema({  
+    nom_utilisateur: { type: String, require: true },
+    prenom_utilisateur: { type: String, require: true },
+    email_utilisateur: { type: String, require: true },
+    motdepasse_utilisateur: { type: String, require: true }
+})
+
+//Exportation du Schema de données
+module.exports = mongoose.model('Utilisateur', utilisateurSchema);
